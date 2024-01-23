09cc1e9e225ac5db42952d6b440be3791d18fb6d
nahem <nahemseguias@gmail.com>
2024-01-23 16:41:20 +0100

    fixed bug on enroll.ts as function signature was inverted

--- Nahem.Q1.Sui.PreR/enrollment/enroll.ts
+++ Nahem.Q1.Sui.PreR/enrollment/enroll.ts
@@ -5,28 +5,28 @@ import { TransactionBlock } from '@mysten/sui.js/transactions';
 import wallet from "../.env/wba-wallet.json";
 import { fromHEX } from "@mysten/bcs";
 
-const enrollment_object_id = "";
-const cohort = "";
+const enrollment_object_id = "0x5927f2574f0a5e2afa574e24bca462269d31cf29bdd2215d908b90b691ea5747";
+const cohort = "0xa85910892fca1bedde91ec6a1379bcf71f4106adbe390ccd67fb696c802d99ab";
 
 // We're going to import our keypair from the wallet file
 const keypair = Ed25519Keypair.fromSecretKey(fromHEX(wallet.privateKey));
 
-// Create a devnet client
-const client = new SuiClient({ url: getFullnodeUrl("devnet") });
+// Create a testnet client
+const client = new SuiClient({ url: getFullnodeUrl("testnet") });
 
 const txb = new TransactionBlock();
 
 // Github account
-const github = new Uint8Array(Buffer.from("-your github account-"));
+const github = new Uint8Array(Buffer.from("nseguias"));
 let serialized_github = txb.pure(bcs.vector(bcs.u8()).serialize(github));
 
 let enroll = txb.moveCall({
     target: `${enrollment_object_id}::enrollment::enroll`,
-    arguments: [serialized_github, txb.object(cohort)],
+    arguments: [txb.object(cohort), serialized_github],
 });
 
 (async () => {
     let txid = await client.signAndExecuteTransactionBlock({ signer: keypair, transactionBlock: txb });
     console.log(`Success! Check our your TX here:
-https://suiexplorer.com/txblock/${txid.digest}?network=devnet`);
+https://suiexplorer.com/txblock/${txid.digest}?network=testnet`);
 })();
--- Nahem.Q1.Sui.PreR/enrollment/transfer.ts
+++ Nahem.Q1.Sui.PreR/enrollment/transfer.ts
@@ -2,7 +2,7 @@ import { getFullnodeUrl, SuiClient } from "@mysten/sui.js/client";
 import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
 import { TransactionBlock } from '@mysten/sui.js/transactions';
 
-import wallet from "./dev-wallet.json"
+import wallet from "../.env/dev-wallet.json"
 
 // Import our dev wallet keypair from the wallet file
 const keypair = Ed25519Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -10,8 +10,8 @@ const keypair = Ed25519Keypair.fromSecretKey(new Uint8Array(wallet));
 // Define our WBA SUI Address
 const to = "0xbf9470d0b09d0144b538cef68f7620d178abf4806ee9d5c9afaf86c80e06d76d";
 
-//Create a Sui devnet client
-const client = new SuiClient({ url: getFullnodeUrl("devnet")});
+//Create a Sui testnet client
+const client = new SuiClient({ url: getFullnodeUrl("testnet")});
 
 (async () => {
     try {
@@ -21,7 +21,7 @@ const client = new SuiClient({ url: getFullnodeUrl("devnet")});
         txb.transferObjects([txb.gas], to);
         let txid = await client.signAndExecuteTransactionBlock({ signer: keypair, transactionBlock: txb });
         console.log(`Success! Check our your TX here:
-        https://suiexplorer.com/txblock/${txid.digest}?network=devnet`);
+        https://suiexplorer.com/txblock/${txid.digest}?network=testnet`);
     } catch(e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
